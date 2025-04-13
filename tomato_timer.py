da3f9b5791862d35e329307ce99f74950ded1a8b
Jeanne Endres <jeanne.endres@gmx.de>
2025-04-13 14:51:29 -0400

    add options

--- tomato_timer.py
+++ tomato_timer.py
@@ -3,11 +3,24 @@ import rumps
 class TomatoTimer(rumps.App):
     def __init__(self):
         super().__init__("🍅")
-        self.menu = ["Start"]
+        self.menu = [
+            "Start Tomato",
+            "Start Break",
+            "Pause",
+            None        
+            ]
 
-    @rumps.clicked("Start")
-    def start_timer(self, _):
-        print("Start clicked")
+    @rumps.clicked("Start Tomato")
+    def start_tomato(self, _):
+        print("Start Tomato")
+
+    @rumps.clicked("Start Break")
+    def start_break(self, _):
+        print("Pause clicked")
+        
+    @rumps.clicked("Pause")
+    def pause(self, _):
+        print("Pause")    
 
 if __name__ == "__main__":
     TomatoTimer().run()
