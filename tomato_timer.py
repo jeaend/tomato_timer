1ba67d7d94153e86a8fccc3dcd36969a18eb2b77
Jeanne Endres <jeanne.endres@gmx.de>
2025-04-13 15:16:07 -0400

    fix break/resume logic

--- tomato_timer.py
+++ tomato_timer.py
@@ -6,13 +6,15 @@ class TomatoTimer(rumps.App):
         self.menu = [
             "Start Tomato",
             "Start Break",
-            "Pause",
+            "Pause / Resume",
             None        
             ]
     
         self.timer = rumps.Timer(self.on_tick, 1)
         self.remaining = 0
         self.running = False
+        self.tomato_active = False
+        self.break_active = False
 
     def on_tick(self, _):
         if self.remaining > 0:
@@ -23,35 +25,48 @@ class TomatoTimer(rumps.App):
         else:
             self.timer.stop()
             self.running = False
+            self.tomato_active = False
+            self.break_active = False
             print("Timer done")
 
     # 25 minutes
     @rumps.clicked("Start Tomato")
     def start_tomato(self, _):
-        print("Start Tomato")
-        self.remaining = 25 * 60  
-        self.running = True
+        if not self.tomato_active:
+            print("Start Tomato")
+            self.remaining = 25 * 60  
+            self.tomato_active = True
+            self.break_active = False
+        else:  
+            print("Resume Tomato")
+
+        self.running = True    
         self.timer.start()
 
     # 5 minutes
     @rumps.clicked("Start Break")
     def start_break(self, _):
-        print("Pause clicked")
-        self.remaining = 5 * 60  
-        self.running = True
-        self.timer.start()
+        if not self.break_active:
+            print("Starting break")
+            self.remaining = 5 * 60
+            self.break_active = True
+            self.tomato_active = False
+        else:
+            print("Resuming break")
 
-    @rumps.clicked("Pause")
+    @rumps.clicked("Pause / Resume")
     def pause(self, _):
         if self.running:
             print("Pause") 
             self.timer.stop()
             self.running = False
         else:
-            print("Resumed")
-            self.timer.start()
-            self.running = True
-
+            if self.tomato_active or self.break_active:       
+                print("Resumed")
+                self.timer.start()
+                self.running = True
+            else:
+                print("Nothing to resume")
 
 if __name__ == "__main__":
     TomatoTimer().run()
