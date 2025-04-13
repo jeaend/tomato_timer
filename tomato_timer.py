fde0fdfb6f2b6d3ef5a8250d36a70372866a4106
Jeanne Endres <jeanne.endres@gmx.de>
2025-04-13 16:19:16 -0400

    add pause/resume logic

--- tomato_timer.py
+++ tomato_timer.py
@@ -3,11 +3,14 @@ import rumps
 class Tomato_Timer(rumps.App):
     def __init__(self):
         super().__init__("🍅")
+        self.pause_resume_item = rumps.MenuItem("Pause", callback=self.pause)
+
         self.menu = [
             "Start Tomato",
             "Start Break",
-            "Pause / Resume",
-            None]
+            self.pause_resume_item,
+            None
+        ]
     
         self.timer = rumps.Timer(self.on_second, 1)
         self.remaining = 0
@@ -24,49 +27,54 @@ class Tomato_Timer(rumps.App):
             self.running = False
             self.tomato_active = False
             self.break_active = False
+            self.pause_resume_item.title = "Pause"  
             rumps.notification("Timer Done", "TOMATOOOW", "The tomato is finished!")
-    
+
     def update_title(self):
         mins, secs = divmod(self.remaining, 60)
         label = "🍅" if self.tomato_active else "☕"
         self.title = f"{label} {mins:02}:{secs:02} {label}"
 
-    # 25 minutes
     @rumps.clicked("Start Tomato")
     def start_tomato(self, _):
         if not self.tomato_active:
             print("Start Tomato")
-            self.remaining = 4 #25 * 60  
+            self.remaining = 4  # 25 * 60
             self.tomato_active = True
             self.break_active = False
+            self.pause_resume_item.title = "Pause"  
         
         self.update_title()
-        self.running = True    
+        self.running = True
+        self.pause_resume_item.set_callback(self.pause)
         self.timer.start()
 
-    # 5 minutes
     @rumps.clicked("Start Break")
     def start_break(self, _):
         if not self.break_active:
             print("Starting break")
-            self.remaining = 2#5 * 60
+            self.remaining = 10  # 5 * 60
             self.break_active = True
             self.tomato_active = False
+            self.pause_resume_item.title = "Pause"  
+        
         self.update_title()
         self.running = True
+        self.pause_resume_item.set_callback(self.pause)
         self.timer.start()
 
-    @rumps.clicked("Pause / Resume")
     def pause(self, _):
         if self.running:
-            print("Pause") 
+            print("Pause")
             self.timer.stop()
             self.running = False
+            self.pause_resume_item.title = "Resume"
         else:
-            if self.tomato_active or self.break_active:       
+            if self.tomato_active or self.break_active:
                 print("Resumed")
                 self.timer.start()
                 self.running = True
+                self.pause_resume_item.title = "Pause"
             else:
                 print("Nothing to resume")
 
