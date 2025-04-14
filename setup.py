eacb7ac7acb91769d6540f7cd9b11e8c2b76eb30
Jeanne Endres <jeanne.endres@gmx.de>
2025-04-13 20:49:02 -0400

    add reqirements, fix build errors

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup
 
 APP = ['tomato_timer.py']
-DATA_FILES = []
+
 OPTIONS = {
-    'argv_emulation': True,
-    'iconfile': 'tomato_icon.icns',  # ← fixed line
+    'argv_emulation': False,
+    'iconfile': 'icon.icns',  
     'plist': {
         'CFBundleName': 'Tomato Timer',
         'CFBundleDisplayName': 'Tomato Timer',
         'CFBundleIdentifier': 'com.jeanne.tomato',
         'CFBundleVersion': '0.1.0',
-        'LSUIElement': True,  # hides dock icon
+        'LSUIElement': True
     },
+    'packages': ['rumps'],
+    'frameworks': []
 }
 
 setup(
     app=APP,
-    name='Tomato Timer',
-    data_files=DATA_FILES,
+    data_files=[],
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
